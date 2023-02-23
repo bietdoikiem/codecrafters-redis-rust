88a5c344f9a7c043e43827d0b34c3a4d26f92973
Lavanya.AM <Lavanya.AM@extcom.com>
2023-02-23 13:16:30 +0700

    test stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
-use std::net::TcpListener;
+mod utils;
+
+use crate::utils::{deserialize_array_command, get_stream_input_str};
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+
+fn encode_resp_simple_string(s: &str) -> Vec<u8> {
+    let mut encoded = vec![];
+    encoded.push(b'+');
+    encoded.extend(s.as_bytes());
+    encoded.push(b'\r');
+    encoded.push(b'\n');
+    encoded
+}
+
+fn handle_client(stream: &mut TcpStream) -> std::io::Result<()> {
+    let response = encode_resp_simple_string("PONG");
+    stream.write_all(&response)?;
+    Ok(())
+}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection")
+            Ok(mut stream) => {
+                println!("accepted new connection");
+                let user_input = get_stream_input_str(&mut stream);
+                match user_input {
+                    Some(input) => {
+                        let cmd_array = deserialize_array_command(&input).unwrap();
+                        let cmd = cmd_array[0].clone().unwrap();
+                        if cmd == "PING" {
+                            stream.write("+PONG\r\n".as_bytes()).unwrap();
+                        } else {
+                            stream
+                                .write("-UNKNOWN_COMMAND_TYPE\r\n".as_bytes())
+                                .unwrap();
+                        }
+                    }
+                    None => {
+                        println!("invalid input");
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
---
+++ src/utils.rs
@@ -0,0 +1,137 @@
+use std::io::Read;
+use std::net::TcpStream;
+
+const CARRIAGE_RETURN: char = '\r';
+const ARRAY_DENOTE: char = '*';
+const BULK_STRING_DENOTE: char = '$';
+
+/// Get input string from TCP stream
+///
+/// # Arguments
+///
+/// * `stream` - TCP Stream
+///
+/// # Returns
+///
+/// The string from stream's buffer
+pub fn get_stream_input_str(stream: &mut TcpStream) -> Option<String> {
+    let mut buffer: [u8; 1024] = [0; 1024];
+    match stream.read(&mut buffer) {
+        Ok(size) => {
+            let input_cow_str = String::from_utf8_lossy(&buffer[..size]);
+            let input_owned_str = input_cow_str.into_owned();
+            return Some(input_owned_str);
+        }
+        Err(e) => {
+            println!("error parsing input: {}", e);
+            return None;
+        }
+    }
+}
+
+/// Deserialize array command
+pub fn deserialize_array_command(cmd: &String) -> Option<Vec<Option<String>>> {
+    let cmd_len = cmd.len();
+    if cmd_len == 0 {
+        return None;
+    }
+
+    let mut cmd_array: Vec<Option<String>> = vec![];
+
+    // Flow-control pointer vars
+    let mut cur_idx = 0;
+    let mut cmd_iterator = cmd.chars();
+    let mut parsing_array_len = false;
+    let mut parsing_array_content = false;
+    let mut parsing_bulk_string_len = false;
+    let mut array_lower_bound = 0;
+    let mut array_prefix_len = -1;
+    let mut bulk_string_prefix_len = -1;
+    let mut bulk_string_len_lower_bound = 0;
+
+    while cur_idx < cmd_len {
+        let cur_char = cmd_iterator.next().unwrap();
+        match cur_char {
+            ARRAY_DENOTE => {
+                parsing_array_len = true;
+                array_lower_bound = cur_idx + 1; // Next char
+            }
+            CARRIAGE_RETURN => {
+                if array_prefix_len == 0 {
+                    break;
+                }
+                if parsing_array_len {
+                    let prefix_length_str = &cmd[array_lower_bound..cur_idx];
+                    match prefix_length_str.parse::<i64>() {
+                        Ok(val) => {
+                            array_prefix_len = val;
+                            if array_prefix_len == -1 {
+                                return None;
+                            }
+                        }
+                        Err(e) => {
+                            println!("error parsing integer: {}", e)
+                        }
+                    }
+                    parsing_array_len = false;
+                } else if parsing_bulk_string_len {
+                    let bulk_string_prefix_len_str = &cmd[bulk_string_len_lower_bound..cur_idx];
+                    match bulk_string_prefix_len_str.parse::<i64>() {
+                        Ok(val) => {
+                            bulk_string_prefix_len = val;
+                        }
+                        Err(e) => {
+                            println!("error parsing integer: {}", e)
+                        }
+                    }
+                    // If got the length
+                    parsing_bulk_string_len = false;
+                    parsing_array_content = true;
+                }
+
+                // Skip next LF
+                cmd_iterator.next();
+                cur_idx += 1;
+            }
+            BULK_STRING_DENOTE => {
+                bulk_string_len_lower_bound = cur_idx + 1;
+                parsing_bulk_string_len = true;
+            }
+            _ => {
+                if parsing_array_content {
+                    if bulk_string_prefix_len == -1 {
+                        cmd_array.push(None);
+                    } else {
+                        let content_slice =
+                            &cmd[cur_idx..cur_idx + bulk_string_prefix_len as usize];
+
+                        cmd_array.push(Some(content_slice.to_string()));
+
+                        // Skip processed bulk string prefix size
+                        cur_idx += bulk_string_prefix_len as usize;
+                        for _ in 0..=bulk_string_prefix_len - 1 {
+                            cmd_iterator.next();
+                        }
+                        parsing_array_content = false;
+                    }
+                }
+            }
+        };
+        cur_idx += 1;
+    }
+    Some(cmd_array)
+}
+
+#[cfg(test)]
+mod utils_tests {
+    use super::deserialize_array_command;
+
+    #[test]
+    fn test_deserialize_array_command_successfully() {
+        let test_cmd = String::from("*3\r\n$4\r\nPING\r\n$4\r\nPONG\r\n");
+        let expect_array: Vec<Option<String>> =
+            vec![Some(String::from("PING")), Some(String::from("PONG"))];
+        let cmd_array = deserialize_array_command(&test_cmd);
+        assert_eq!(expect_array, cmd_array.unwrap());
+    }
+}
